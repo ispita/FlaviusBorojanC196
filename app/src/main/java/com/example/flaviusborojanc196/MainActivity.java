521d487061fe7f595d0220d7e722be6691bf993f
ispita <stop@askingme.com>
2020-02-17 15:36:35 -0600

    Basic alert functionality achieved, need to tie assessments/courses/terms to alert system

--- app/src/main/java/com/example/flaviusborojanc196/MainActivity.java
+++ app/src/main/java/com/example/flaviusborojanc196/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.flaviusborojanc196;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,6 +8,8 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,17 +21,23 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
+import static com.example.flaviusborojanc196.FlaviusBorojanC196.CHANNEL_1_ID;
+import static com.example.flaviusborojanc196.FlaviusBorojanC196.CHANNEL_2_ID;
+
 public class MainActivity extends AppCompatActivity {
     public static final int ADD_TERM_REQUEST = 1;
     public static final int ADD_COURSE_REQUEST = 1;
     public static final int ADD_ASSESSMENT_REQUEST = 1;
-
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
 
 
@@ -36,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
 
 
         Button buttonViewTerm = findViewById(R.id.view_term_button);
@@ -72,6 +85,38 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void sendOnChannel1(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_save)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_delete)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        notificationManager.notify(2, notification);
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
